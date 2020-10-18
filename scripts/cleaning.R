f1695e8f76adc3e0e7330370af5eff19dc437a30
asmae-toumi <atoumi.cu@gmail.com>
2020-10-18 18:13:21 -0400

    adding game minutes and offensive team leading

--- scripts/cleaning.R
+++ scripts/cleaning.R
@@ -50,6 +50,45 @@ plays <- plays %>%
 
 # TO DO: join player ID
 
+plays <- plays %>% 
+  separate(game_clock, into = c("minutes", "seconds")) %>% 
+  mutate(minutes = as.numeric(minutes), 
+         seconds = as.numeric(seconds),
+         seconds_left_quarter = 60*minutes + seconds,
+         seconds_left_game = seconds_left_quarter + (4-quarter)*15*60,
+         seconds_into_game = 3600-seconds_left_game, 
+         game_minute = floor(seconds_into_game/60)) 
+
+plays <- plays %>% 
+  left_join(games, by = "game_id")
+
+plays <- plays %>% 
+  mutate(
+    score_diff = abs(pre_snap_home_score - pre_snap_visitor_score),
+    game_close = case_when(
+      score_diff <= 16 & game_minute <= 45 ~ "yes", 
+      score_diff <= 8 & game_minute <= 15 ~ "no", 
+      TRUE ~ "not close"
+    ), 
+    leading = case_when(
+      pre_snap_home_score < pre_snap_visitor_score ~ "home",
+      pre_snap_visitor_score < pre_snap_home_score ~ "away",
+      TRUE ~ "tie"
+    ),
+    offensive_team_home = case_when(
+      possession_team == home_team_abbr ~ "yes", 
+      possession_team == visitor_team_abbr ~ "no", 
+      TRUE ~ "other"
+    ),
+    offensive_team_leading = case_when(
+      leading == "tie" ~ "no", 
+      leading == "home" & offensive_team_home == "yes" ~ "yes", 
+      leading == "home" & offensive_team_home == "no" ~ "no", 
+      leading == "away" & offensive_team_home == "yes" ~ "no", 
+      leading == "away" & offensive_team_home == "no" ~ "yes"
+    )
+  ) 
+
 
 # Players -----------------------------------------------------------------
 
@@ -69,8 +108,6 @@ all_weeks <- all_weeks %>%
   mutate(x = ifelse(play_direction == "left", 120-x, x),
          y = ifelse(play_direction == "left", 160/3 - y, y))
 
-
-#TO DO: gg_football()
 
 
 
