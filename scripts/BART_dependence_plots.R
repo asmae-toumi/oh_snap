6c43482bc219bd838d456fe1a2fb45aedbbca6d1
aq86 <atoumi@mgh.harvard.edu>
2020-12-26 20:32:23 -0500

    adding more plot examples

--- scripts/BART_dependence_plots.R
+++ scripts/BART_dependence_plots.R
@@ -33,6 +33,64 @@ trained_plus_phat_time_of_throw %>%
   theme(legend.title = element_blank()) + 
   scale_y_continuous(breaks = scales::pretty_breaks())
 
+trained_plus_phat_time_of_throw %>%
+  filter(number_of_pass_rushers %in% 2:7) %>% 
+  ggplot(aes(time_to_throw, prob_means, color = as.factor(number_of_pass_rushers))) + 
+  geom_smooth(method = "loess", se = FALSE) + 
+  geom_smooth(aes(time_to_throw, low_bound), method = "loess", lty = 3, se = FALSE) + 
+  geom_smooth(aes(time_to_throw, upp_bound), method = "loess", lty = 3, se = FALSE) + 
+  geom_hline(aes(yintercept = 0)) + 
+  theme_minimal() + 
+  labs(title = "Time of throw model",
+       subtitle = "time_to_throw by # pass rushers",
+       x = "time_to_throw",
+       y = "Completion probability") +
+  theme(legend.title = element_blank()) + 
+  scale_y_continuous(breaks = scales::pretty_breaks())
+
+trained_plus_phat_time_of_throw %>% 
+  filter(number_of_pass_rushers %in% 2:7) %>% 
+  group_by(number_of_pass_rushers) %>% 
+  summarise(ave_prob = mean(prob_means), 
+            se_prob = sd(prob_means)/sqrt(n())) %>% 
+  ggplot(aes(number_of_pass_rushers, ave_prob)) + 
+  geom_bar(stat = "identity", width=0.3, fill = "#132f3c") +
+  scale_y_continuous(breaks = scales::pretty_breaks()) +
+  labs(
+    y = "Completion probability", 
+    x = "# pass rushers") +
+  theme_minimal()
+
+trained_plus_phat_time_of_throw %>% 
+  group_by(qb_hit) %>% 
+  summarise(ave_prob = mean(prob_means), 
+            sd = sd(prob_means),
+            se_prob = sd(prob_means)/sqrt(n())) %>% 
+  ggplot(aes(qb_hit, ave_prob)) + 
+  geom_bar(stat = "identity", width=0.3, fill = "#FFA3AF") +
+  geom_errorbar(aes(ymin=ave_prob-sd, ymax=ave_prob+sd), 
+                color = "#132f3c", width = 0.05) +
+  scale_y_continuous(breaks = scales::pretty_breaks()) +
+  labs(
+    y = "Completion probability", 
+    x = "qb_hit") +
+  theme_minimal()
+
+trained_plus_phat_time_of_throw %>% 
+  group_by(is_leading) %>% 
+  summarise(ave_prob = mean(prob_means), 
+            sd = sd(prob_means),
+            se_prob = sd(prob_means)/sqrt(n())) %>% 
+  ggplot(aes(is_leading, ave_prob)) + 
+  geom_bar(stat = "identity", width=0.3, fill = "#FFA3AF") +
+  geom_errorbar(aes(ymin=ave_prob-sd, ymax=ave_prob+sd), 
+                color = "#132f3c", width = 0.05) +
+  scale_y_continuous(breaks = scales::pretty_breaks()) +
+  labs(
+    y = "Completion probability", 
+    x = "is_leading") +
+  theme_minimal()
+
 # Example of plot with trained_plus_phat_time_of_arrival data 
 
 trained_plus_phat_time_of_arrival %>% 
