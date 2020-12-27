e77b69927f6d1e03f8f578043b2c6805cba9bb28
aq86 <atoumi@mgh.harvard.edu>
2020-12-26 19:53:33 -0500

    adding plots and fixing typo

--- scripts/BART_dependence_plots.R
+++ scripts/BART_dependence_plots.R
@@ -2,8 +2,8 @@ library(tidyverse)
 
 # load trained_plus_phat_time_of_throw and trained_plus_phat_time_of_throw
 
-trained_plus_phat_time_of_throw <- load("data/BART_time_of_throw/trained_plus_phat_time_of_throw")
-trained_plus_phat_time_of_throw <- load("data/BART_time_of_arrival/trained_plus_phat_time_of_arrival")
+load("data/BART_time_of_throw/trained_plus_phat_time_of_throw.RData")
+load("data/BART_time_of_arrival/trained_plus_phat_time_of_arrival.RData")
 
 # Example of plot with trained_plus_phat_time_of_throw data 
 
@@ -16,6 +16,19 @@ trained_plus_phat_time_of_throw %>%
   theme_minimal() + 
   labs(title = "Time of throw model",
        x = "angle_diff",
+       y = "Completion probability") +
+  theme(legend.title = element_blank()) + 
+  scale_y_continuous(breaks = scales::pretty_breaks())
+
+trained_plus_phat_time_of_throw %>% 
+  ggplot(aes(humidity, prob_means, color = roof)) + 
+  geom_smooth(method = "loess", se = FALSE) + 
+  geom_smooth(aes(humidity, low_bound), method = "loess", lty = 3, se = FALSE) + 
+  geom_smooth(aes(humidity, upp_bound), method = "loess", lty = 3, se = FALSE) + 
+  geom_hline(aes(yintercept = 0)) + 
+  theme_minimal() + 
+  labs(title = "Time of throw model",
+       x = "humidity",
        y = "Completion probability") +
   theme(legend.title = element_blank()) + 
   scale_y_continuous(breaks = scales::pretty_breaks())
