6c7f9a020aa9c9a5b1ccd2f535e4ea846b7d467b
Tony ElHabr <anthonyelhabr@gmail.com>
2021-02-20 19:50:31 -0600

    add new ranking tables

--- scripts/final_rankings.R
+++ scripts/final_rankings.R
@@ -1,5 +1,9 @@
 
 source('scripts/target_prob_setup.R')
+# pak::pkg_install("jthomasmock/espnscrapeR")
+teams <- espnscrapeR::get_nfl_teams() %>% select(team = team_short_name, logo)
+teams
+
 tpoe <- file.path('data', 'target_prob', 'small', 'tpoe_player_rankings.csv') %>% readr::read_csv()
 cpoe <- file.path('data', 'catch_prob', 'dpoe_arrival_player_rankings.csv') %>% readr::read_csv()
 # roster <- nflfastR::fast_scraper_roster(seasons = 2018)
@@ -37,88 +41,165 @@ xpoe_wide <-
   group_by(grp) %>% 
   mutate(grp_rnk = row_number(grp_rnk_t + grp_rnk_c)) %>% 
   ungroup() %>% 
-  arrange(grp, grp_rnk)
+  arrange(grp, grp_rnk) %>% 
+  left_join(teams)
 xpoe_wide
 
-colors <-
-  c('#ffa3af',
-    '#bf83a8',
-    '#7a6991',
-    '#3d4d6a',
-    '#132f3c')
-tb <-
+defenders_top_pff <-
+  tibble(
+    display_name = c('Stephon Gilmore', 'Desmond King', 'Chris Harris', 'Kareem Jackson', 'Byron Jones', 'Jason McCourty', 'Kyle Fuller', 'Patrick Peterson', 'Bryce Callahan', 'Johnathan Joseph', 'Prince Amukamara', 'Denzel Ward', 'Marlon Humphrey', 'Casey Hayward', 'Pierre Desir', 'Xavien Howard', 'A.J. Bouye', 'Darius Slay', 'Trumaine Johnson', 'Marshon Lattimore', 'Steven Nelson', 'William Jackson', 'Adoree\' Jackson', 'Jalen Ramsey', 'Jaire Alexander')
+  ) %>% 
+  mutate(rnk_pff = row_number())
+defenders_top_pff
+
+# maxes <-
+#   xpoe_wide %>% 
+#   filter(grp_rnk <= 10) %>% 
+#   summarize(across(matches('grp_rnk'), list(max = max), na.rm = TRUE))
+# maxes
+
+xpoe_wide_filt <-
   xpoe_wide %>% 
   filter(grp_rnk <= 10 | display_name %in% c('Jonathan Jones', 'Jason McCourty', 'Patrick Chung', 'Devin McCourty')) %>% 
-  # arrange(grp, grp_rnk) %>% 
-  mutate(label = sprintf('%s (%s)', display_name, team)) %>% 
   select(
-    label,
-    # display_name,
-    # team,
-    # grp,
-    xpoe_t,
-    xpoe_c,
+    # label,
+    display_name,
+    grp,
+    team,
+    logo,
     grp_rnk_t,
     grp_rnk_c,
     grp_rnk
-  ) %>% 
-  gt::gt(rowname_col = 'label') %>% 
-  gt::cols_label(
-    .list = 
-      list(
-        # display_name = gt::md('**Name (Team)**'),
-        # team = gt::md('**Team**'),
-        # grp = gt::md('**Group**'),
-        xpoe_t = gt::md('**dTPOE**'),
-        xpoe_c = gt::md('**dCPOE**'),
-        grp_rnk_t = gt::md('**dTPOE Rank**'),
-        grp_rnk_c = gt::md('**dCPOE Rank**'),
-        grp_rnk = gt::md('**Group Rank**')
-      )
-  ) %>% 
-  gt::data_color(
-    columns = vars(xpoe_t),
-    colors = scales::col_numeric(
-      palette = colors,
-      # palette = as.character(paletteer::paletteer_c('scico::berlin', n = 5, direction = -1)),
-      domain = c(5, -21)
-    )
-  ) %>% 
-  gt::data_color(
-    columns = vars(xpoe_c),
-    colors = scales::col_numeric(
-      palette = colors,
-      # palette = rev(c('#ffffff', '#f2fbd2', '#c9ecb4', '#93d3ab', '#35b0ab')),
-      
-      # palette = as.character(paletteer::paletteer_c('scico::berlin', n = 5, direction = -1)),
-      domain = c(6, -6)
-    )
-  ) %>% 
-  gt::tab_row_group(
-    group = 'Safety', # gt::md('Safety'),
-    rows = 13:24
-  ) %>% 
-  gt::tab_row_group(
-    group = 'Cornerback',
-    rows = 1:12
-  ) %>% 
-  gt::fmt_number(columns = vars(xpoe_t), decimals = 1) %>% 
-  gt::fmt_number(columns = vars(xpoe_c), decimals = 2) %>% 
-  gt::tab_footnote(
-    locations = gt::cells_column_labels(
-      columns = vars(grp_rnk)
-    ),
-    footnote = 'NE players included (regardless of rank) for comparison.'
-  ) %>% 
-  gt::tab_source_note(
-    source_note = gt::md('{nflfastR} served as the source for position groups and teams.')
-  ) %>% 
-  gt::tab_header(
-    title = gt::md('**Top 10 Cornerback and Safeties**'),
-    subtitle = 'Composite Rank of dTPOE and dCPOE Ranks'
-  ) %>% 
-  gt::tab_options(
-    row_group.font.weight = 'bold'
   )
-tb
-gt::gtsave(tb, filename = file.path('figs', 'final_player_rankings.png'))
+
+xpoe_wide_pff <-
+  xpoe_wide %>% 
+  inner_join(defenders_top_pff) %>% 
+  select(
+    display_name,
+    team,
+    logo,
+    grp_rnk_t,
+    grp_rnk_c,
+    grp_rnk,
+    rnk_pff
+  ) %>% 
+  arrange(rnk_pff)
+
+.get_valid_grps <- function() {
+  c('CB', 'S')
+}
+.validate_grp <- function(x = .get_valid_grps(), ...) {
+  match.arg(x, ...)
+}
+
+grps <- .get_valid_grps()
+.do_gt_by_grp <- function(grp = .get_valid_grps()) {
+  .validate_grp(grp)
+  grp_lab <- switch(grp, 'S' = 'Safeties', 'CB' = 'Cornerbacks')
+  res <-
+    xpoe_wide_filt %>% 
+    filter(grp == !!grp) %>% 
+    select(-grp) %>% 
+    gt::gt() %>% 
+    gt::cols_label(
+      .list = 
+        list(
+          display_name = 'Name',
+          team = 'Team',
+          logo = ' ',
+          grp_rnk_t = 'Coverage Rank',
+          grp_rnk_c = 'Contest Rank',
+          grp_rnk = gt::md('**Group Rank**')
+        )
+    ) %>% 
+    gt::tab_footnote(
+      locations = gt::cells_column_labels(
+        columns = vars(grp_rnk)
+      ),
+      footnote = 'NE players included (regardless of rank) for comparison.'
+    ) %>% 
+    gt::text_transform(
+      locations = gt::cells_body(
+        vars(logo)
+      ),
+      fn = function(x) {
+        gt::web_image(
+          url = x,
+          height = 25
+        )
+      }
+    ) %>% 
+    gt::tab_source_note(
+      source_note = gt::md('{nflfastR} is source for position groups and teams.')
+    ) %>% 
+    gt::tab_header(
+      title = gt::md(glue::glue('**Top 10 {grp_lab}**')),
+      subtitle = 'Composite Rank of Coverage and Contest Ranks'
+    ) %>% 
+    gt::tab_options(
+      row_group.font.weight = 'bold'
+    )
+  gt::gtsave(res, filename = file.path('figs', glue::glue('final_{tolower(grp)}_rankings.png')))
+  res
+}
+
+.get_valid_pages <- function() {
+  c('1', '2')
+}
+.validate_grp <- function(x = .get_valid_pages(), ...) {
+  match.arg(x, ...)
+}
+
+pages <- .get_valid_pages()
+.do_gt_pff <- function(page = c('1', '2')) {
+  # .validate_page(page)
+  if(page == '1') {
+    page_min <- 1
+    page_max <- 12
+    page_lab <- '1-12'
+  } else {
+    page_min <- 13
+    page_max <- 24
+    page_lab <- '13-24'
+  }
+  res <-
+    xpoe_wide_pff %>% 
+    filter(rnk_pff >= page_min & rnk_pff <= page_max) %>% 
+    gt::gt() %>% 
+    gt::cols_label(
+      .list = 
+        list(
+          display_name = 'Name',
+          team = 'Team',
+          logo = ' ',
+          grp_rnk_t = 'Coverage Rank',
+          grp_rnk_c = 'Contest Rank',
+          grp_rnk = gt::md('**Group Rank**'),
+          rnk_pff = gt::md('**PFF Rank**')
+        )
+    ) %>% 
+    gt::text_transform(
+      locations = gt::cells_body(
+        vars(logo)
+      ),
+      fn = function(x) {
+        gt::web_image(
+          url = x,
+          height = 25
+        )
+      }
+    ) %>% 
+    gt::tab_header(
+      title = gt::md(glue::glue('**Top {page_lab} PFF Cornerbacks**')),
+      subtitle = 'Composite Rank of Coverage and Contest Ranks'
+    ) %>% 
+    gt::tab_options(
+      row_group.font.weight = 'bold'
+    )
+  gt::gtsave(res, filename = file.path('figs', glue::glue('final_rankings_pff_{page_lab}.png')))
+  res
+}
+
+# grps %>% walk(.do_gt_by_grp)
+pages %>% walk(.do_gt_pff)
